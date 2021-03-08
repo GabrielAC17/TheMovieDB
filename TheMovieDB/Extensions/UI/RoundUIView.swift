d0f319895fb55a47494192ee0c0b8b98b2076002
Gabriel Cordeiro <gabriel.cordeiro@electrolux.com>
2021-03-08 13:58:23 -0300

    Partial App

---
+++ TheMovieDB/Extensions/UI/RoundUIView.swift
@@ -0,0 +1,31 @@
+//
+//  RoundUIView.swift
+//  TheMovieDB
+//
+//  Created by retina on 07/03/21.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundUIView: UIView {
+
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+}
